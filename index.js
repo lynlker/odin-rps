396c3291c5e739347b74a2d72d574b169bde2b06
lynlker <nosrednassel@gmail.com>
2022-07-07 13:00:52 +0200

    Added game function

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 const options = ["rock", "paper", "scissors"];
-
+let playerPoints = 0
+let computerPoints = 0
 
 function computerPlay (){
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == "rock"){
-        if(computerSelection == "rock"){
-            return "Computer chose rock! It's a tie!"
-        } else if (computerSelection == "paper"){
-            return "Computer chose paper! Computer wins this round!"
+function playerPlay(){
+    let choice = "What is your choice? Rock, Paper, or Scissors";
+    return choice.toLowerCase;
+}
+
+function playRound(player, computer){
+    if (player == "rock"){
+        if(computer == "rock"){
+            return "Computer chose rock! It's a tie!";
+        } else if (computer == "paper"){
+            computerPoints++;
+            return "Computer chose paper! Computer wins this round!";
         }else{
-            return "Computer chose scissors! You win this round!"
+            playerPoints++;
+            return "Computer chose scissors! You win this round!";
         }
-    } else if (playerSelection == "paper"){
-        if(computerSelection == "paper"){
-            return "Computer chose paper! It's a tie!"
-        } else if (computerSelection == "scissors"){
-            return "Computer chose scissors! Computer wins this round!"
+    } else if (player == "paper"){
+        if(computer == "paper"){
+            return "Computer chose paper! It's a tie!";
+        } else if (computer == "scissors"){
+            computerPoints++
+            return "Computer chose scissors! Computer wins this round!";
         }else{
-            return "Computer chose rock! You win this round!"
+            playerPoints++
+            return "Computer chose rock! You win this round!";
         }
-    } else if (playerSelection == "scissors"){
-        if(computerSelection == "scissors"){
-            return "Computer chose scissors! It's a tie!"
-        } else if (computerSelection == "rock"){
-            return "Computer chose rock! Computer wins this round!"
+    } else if (player == "scissors"){
+        if(computer == "scissors"){
+            return "Computer chose scissors! It's a tie!";
+        } else if (computer == "rock"){
+            computerPoints++
+            return "Computer chose rock! Computer wins this round!";
         }else{
-            return "Computer chose paper! You win this round!"
+            playerPoints++
+            return "Computer chose paper! You win this round!";
         }
     } else{
-        return "owo"
+        computerPoints++
+        return "Choose an actual option, you moron. I'll add a point to the computer for this one.";
+    }
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        playerSelection = playerPlay();
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+
+    }
+    
+    if (playerPoints == computerPoints){
+        console.log("The game ended in a tie!");
+    } else if (playerPoints < computerPoints){
+        console.log("The computer wins the game! Ur a moron");
+    } else {
+        console.log("You win this game! Now go touch some grass.")
     }
 }
